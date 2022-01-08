19b4e933dcf3af19b1c23338153b7d5cfe6df2e1
PrelucaAnca <preluca_anca88@yahoo.com>
2022-01-08 15:39:24 +0200

    Add project files.

---
+++ Preluca_Anca_Lab12/Preluca_Anca_Lab12/Preluca_Anca_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using Preluca_Anca_Lab12.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preluca_Anca_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
